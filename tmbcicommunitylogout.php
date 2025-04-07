015a4c5df269d0bace6dd1fc74a0bfc68efdf501
BlissBCI <uodaccw@gmail.com>
2025-04-07 18:16:11 +0100

    Reverted login to php, updated head of php pages

--- tmbcicommunitylogout.php
+++ tmbcicommunitylogout.php
@@ -8,6 +8,9 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+    <!-- Website CSS -->
+    <link rel="stylesheet" href="https://conceptography.org/style.css">
+
     <!-- JavaScript -->
     <!-- jQuery, Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
@@ -15,9 +18,6 @@
 
     <!-- Responsive Viewport Tag -->
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Website CSS -->
-    <link rel="stylesheet" href="https://conceptography.org/style.css">
 
     <!-- Load icon library -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
